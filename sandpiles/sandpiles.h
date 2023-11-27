0fbdc46c18941408e6972b709c0e4c68d34163a8
joseftango <4279@holbertonschool.com>
2023-11-27 21:51:09 +0100

    update

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -9,4 +9,7 @@ void topple(int grid[SIZE][SIZE], int row, int col);
 int isStable(int grid[SIZE][SIZE]);
 void sandpiles_sum(int grid1[SIZE][SIZE], int grid2[SIZE][SIZE]);
 void printSandpile(int grid[SIZE][SIZE]);
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+
 #endif /* SANDPILES_H */
