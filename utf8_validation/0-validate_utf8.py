0d51fe1c44988ac4ffc9a232f3bed4210fb93c36
joseftango <4279@holbertonschool.com>
2023-12-04 22:03:24 +0100

    0-validate_utf8.py

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""0-validate_utf8 module"""
+
+
+def validUTF8(data):
+    """function that determines if a given data set
+    represents a valid UTF-8 encoding"""
+    num_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        num_bytes -= 1
+
+    return num_bytes == 0
