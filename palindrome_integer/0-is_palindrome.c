418fee9ea2c69f4fece961c03e1dc46aabc2d65d
joseftango <4279@holbertonschool.com>
2023-12-04 20:56:30 +0100

    0-is_palindrome.c

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks whether an unsigned integer is a palindrome 
+ * @n: given integer
+ * Return: 1 or 0
+**/
+
+int is_palindrome(unsigned long n) {
+	unsigned long int reversed = 0, remainder, original;
+	original = n;
+
+	while (n != 0) {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+	if (original == reversed)
+	return (1);
+
+	return (0);
+}
---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,9 @@
+#ifndef PALINDROME_INTEGER
+#define PALINDROME_INTEGER
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int is_palindrome(unsigned long n);
+
+#endif /* LISTS_H */
