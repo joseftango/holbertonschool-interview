4f24aad3328c62c92d2b69e165c653beb41daa92
joseftango <4279@holbertonschool.com>
2023-12-29 21:48:31 +0100

    0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,21 +1,37 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process'); 
-const arg = process.argv[2]; 
+const process = require('process');
 
+const args = process.argv.slice(2);
+if (args < 1) {
+  console.log('Error');
+  process.exit(1);
+}
+
+const arg = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${arg}`;
 
-request(url, (error, response, body) => {
-  if (error) console.log(error);
-  const myobj = JSON.parse(body);
-  const chars = myobj.characters;
+let characters = [];
+request(url, (err, resp, body) => {
+  if (err || resp.statusCode !== 200) console.log(err);
+  else characters = JSON.parse(body).characters;
+  const size = Object.keys(characters).length;
+  const array = Array(size).fill();
+  let data = 0;
+  for (let i = 0; i < size; i++) {
+    request(characters[i], (erro, respo, bodys) => {
+      if (erro || respo.statusCode !== 200) console.log(erro);
+      else {
+        array[i] = JSON.parse(bodys).name;
+        data++;
+      }
+      if (data === size) {
+        for (let j = 0; j < size; j++) {
+          console.log(array[j]);
+        }
+      }
+    }
 
-  for (const u of chars) {
-    request(u, (error, response, body) => {
-      if (error) console.log(error);
-      const obj = JSON.parse(body);
-
-      console.log(obj.name);
-    });
-	}
+    );
+  }
 });
